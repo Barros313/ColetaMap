23e70468e020dc182ebc0bf2a1f69a473887e6b7
Gabriel Barros de Almeida <gbarros1337@gmail.com>
2024-06-27 00:30:24 -0300

    Buildt a new backend server and connected with mongoDB

---
+++ coleta-map_backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGODB_LOCALHOST_URI;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
+app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
