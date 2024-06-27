cf84abc91343fbce6464bb23fbe27fa670b801d9
Gabriel Barros de Almeida <gbarros1337@gmail.com>
2024-06-27 00:33:41 -0300

    Removed useNewUrlParser and useUnifiedTopology parameters
    Signed-off-by: Gabriel Barros de Almeida <gbarros1337@gmail.com>

--- coleta-map_backend/server.js
+++ coleta-map_backend/server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(MONGO_URI)
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.log(err));
 
