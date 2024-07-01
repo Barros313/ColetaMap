831b4255cfe7864a92d436ea5ab3958b5d3944ad
Gabriel Barros de Almeida <gbarros1337@gmail.com>
2024-07-01 10:54:18 -0300

    Added sidebar menu
    Signed-off-by: Gabriel Barros de Almeida <gbarros1337@gmail.com>

---
+++ coleta-map_frontend/components/SidebarMenu.js
@@ -0,0 +1,25 @@
+import 'react-native-gesture-handler'
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { StyleSheet } from 'react-native';
+
+const Drawer = createDrawerNavigator();
+
+import Profile from './screens/Profile';
+import Map from './screens/Map';
+
+export default function SidebarMenu() {
+    return (
+        <NavigationContainer>
+            <Drawer.Navigator initialRouteName="Map">
+                <Drawer.Screen name="Map" component={Map} />
+                <Drawer.Screen name="Profile" component={Profile} />
+            </Drawer.Navigator>
+        </NavigationContainer>
+    );
+}
+
+const styles = StyleSheet.create({
+
+})
