fd631e78049c57fdad8ac9d2ae3f714cc53a21dc
Gabriel Barros de Almeida <gbarros1337@gmail.com>
2024-07-01 12:02:41 -0300

    Added authentication flow
    Signed-off-by: Gabriel Barros de Almeida <gbarros1337@gmail.com>

--- coleta-map_frontend/components/Authentication.js
+++ coleta-map_frontend/components/Authentication.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import { StyleSheet, View } from 'react-native';
 
 import { Login, Register } from './screens/authentication'
 
-export default function Authentication() {
+export default function Authentication({ onLogin }) {
+    const [isLogin, setIsLogin] = useState(true);
+
     return (
-        <>
-            <Register />
-            <Login />
-        </>
-    )
+        <View style={{ flex: 1 }}>
+            {isLogin ? (
+                <Login onLogin={onLogin} onRegister={() => setIsLogin(false)} />
+            ) : (
+                <Register onRegisterSuccess={() => setIsLogin(true)} onBackToLogin={() => setIsLogin(true)} />
+            )}
+        </View>
+    );
 }
