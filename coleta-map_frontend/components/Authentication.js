912c479da25eccc71b488a22b5f3dae66d0357cd
Gabriel Barros de Almeida <gbarros1337@gmail.com>
2024-07-01 11:12:26 -0300

    Added authentication component
    Signed-off-by: Gabriel Barros de Almeida <gbarros1337@gmail.com>

---
+++ coleta-map_frontend/components/Authentication.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { Login, Register } from './screens/authentication'
+
+export default function Authentication() {
+    return (
+        <>
+            <Register />
+            <Login />
+        </>
+    )
+}
