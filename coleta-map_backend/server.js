5acf8abf0b19116aa12b9127370f670865dccb3f
Gabriel Barros de Almeida <gbarros1337@gmail.com>
2024-07-01 10:04:37 -0300

    Updated server.js to use database routes
    Signed-off-by: Gabriel Barros de Almeida <gbarros1337@gmail.com>

--- coleta-map_backend/server.js
+++ coleta-map_backend/server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGODB_LOCALHOST_URI;
+const MONGO_URI = process.env.MONGODB_URI + "/coletamap-app";
 
 app.use(cors());
 app.use(express.json());
@@ -17,5 +17,8 @@ mongoose.connect(MONGO_URI)
 
 const authenticationRoute = require('./routes/authentication');
 app.use('/auth', authenticationRoute);
+
+const databaseRoute = require('./routes/database');
+app.use('/pontos', databaseRoute);
 
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
